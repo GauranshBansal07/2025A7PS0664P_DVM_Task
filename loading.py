a3595c5c4a4010c13d84bcd734748d34f1bf276c
Gauransh Bansal <gauranshbansal338@gmail.com>
2025-10-29 09:10:39 +0530

    Refactor loading functions into Loading class

--- loading.py
+++ loading.py
@@ -3,27 +3,30 @@ import csv
 from Station import Station
 from Ticket import Ticket
 
-def loading_stations_file(filepath):
-    station_dict = {}
-    with open(filepath, 'r') as stations_file:
-        csv_reader = csv.DictReader(stations_file)
+class Loading:
+    @staticmethod
+    def loading_stations_file(filepath):
+        station_dict = {}
+        with open(filepath, 'r') as stations_file:
+            csv_reader = csv.DictReader(stations_file)
+    
+            for line in csv_reader:
+                station_name = line['station_name']
+                line_name = line['line_name']
+    
+                if station_name not in station_dict:
+                    station_dict[station_name] = Station(station_name)
+                station_dict[station_name].add_lines(line_name)
+        return station_dict
 
-        for line in csv_reader:
-            station_name = line['station_name']
-            line_name = line['line_name']
-
-            if station_name not in station_dict:
-                station_dict[station_name] = Station(station_name)
-            station_dict[station_name].add_lines(line_name)
-    return station_dict
-
-def loading_lines_file(filepath):
-    lines_dict = {}
-    with open(filepath, 'r') as lines_file:
-        csv_reader = csv.DictReader(lines_file)
-
-        for line in csv_reader:
-            line_name = line['line_name']
-            stations_list = line['stations_list'].split(',')
-            lines_dict[line_name] = stations_list
-    return lines_dict
+    @staticmethod
+    def loading_lines_file(filepath):
+        lines_dict = {}
+        with open(filepath, 'r') as lines_file:
+            csv_reader = csv.DictReader(lines_file)
+    
+            for line in csv_reader:
+                line_name = line['line_name']
+                stations_list = line['stations_list'].split(',')
+                lines_dict[line_name] = stations_list
+        return lines_dict
